41d3f26945549536f1053ed379c706147dadb66b
mohamed nouhi <mohamednoouuhi@gmail.com>
2024-02-17 13:20:04 +0100

    hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 
-"""A simple Flask web application with two routes: '/' and '/hbnb'."""
+"""A simple Flask web application with two routes: '/' and '/hbnb'"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
-    """Display 'Hello HBNB!'."""
+    """displays Hello HBNB"""
     return "“Hello HBNB!”"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-     """Display 'HBNB'."""
+    """displays HBNB"""
     return "“HBNB”"
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000)
-
