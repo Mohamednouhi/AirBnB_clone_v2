f703f21f6f99560773b8c3cc668e76335543a1df
mohamed nouhi <mohamednoouuhi@gmail.com>
2024-02-17 16:26:29 +0100

    python

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,37 +1,28 @@
 #!/usr/bin/python3
-"""flask web app that runs 4 routes"""
+
+
 from flask import Flask
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-
-@app.route('/')
-def hello_world():
-    """ Returns some text. """
+@app.route('/', strict_slashes=False)
+def hello():
     return 'Hello HBNB!'
 
-
-@app.route('/hbnb')
-def hello():
-    """ Return other text. """
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
     return 'HBNB'
 
-
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """ replace text with variable. """
     text = text.replace('_', ' ')
-    return f"C {text}"
+    return 'C {}'.format(text)
 
-
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_text(text='is cool'):
-    """ replace more text with another variable. """
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
     text = text.replace('_', ' ')
-    return f"Python {text}"
-
+    return 'Python {}'.format(text)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=50
+    app.run(host='0.0.0.0', port=5000)
+
