4aca8297671a6cc56873e5bcf7f288d99b2bf028
mohamed nouhi <mohamednoouuhi@gmail.com>
2024-02-17 13:58:13 +0100

    c

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_world():
     """displays Hello HBNB"""
-    return "“Hello HBNB!”"
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """displays HBNB"""
-    return "“HBNB”"
+    return "HBNB"
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""flask web application that has 3 routes"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """displays  “Hello HBNB!” """
+    return "“Hello HBNB!”"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays hbnb"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """displays c and the text we gave it"""
+    text_variable = text.replace(" ", "_")
+    return f"C {text_variable}"
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=5000)
