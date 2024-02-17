6337ff47ea20571c686eb7552df70140f3c9b0ee
mohamed nouhi <mohamednoouuhi@gmail.com>
2024-02-17 14:15:18 +0100

    python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/bin/bash/python3
+"""flask web app that runs 4 routes"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """'displays  Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays  HBNB!"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """displays C followed by the text"""
+    text_variable = text.replace(' ', '_')
+    return f"C {text_variable}"
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """displays Python followed by the text """
+    text_variable=text.replace(' ', '_')
+    return f"Python {text_variable}"
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=5000)
+
