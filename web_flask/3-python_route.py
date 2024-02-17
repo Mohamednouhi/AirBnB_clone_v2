e4f77a78ea232c77aeffe67eaf86233eaf7816fb
mohamed nouhi <mohamednoouuhi@gmail.com>
2024-02-17 16:28:16 +0100

    python

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 
-
+"""flask web application that has 3 routes"""
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def hello():
-    return 'Hello HBNB!'
+def hello_world():
+    """displays  “Hello HBNB!” """
+    return "“Hello HBNB!”"
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB'
+    """displays hbnb"""
+    return "HBNB"
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+def c(text):
+    """displays c and the text we gave it"""
+    text_variable = text.replace('_', ' ')
+    return f"C {text_variable}"
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -23,6 +26,6 @@ def python_text(text):
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
 
-if __name__ == '__main__':
+if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000)
 
