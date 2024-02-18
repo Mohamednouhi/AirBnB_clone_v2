c7c6290c2bd31d77c00d4fc7d0e886e39416342a
mohamed nouhi <mohamednoouuhi@gmail.com>
2024-02-18 16:30:42 +0100

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,5 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    def close(self):
+        self.reload()
